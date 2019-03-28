bc0c790d721a1711699b6c3ec0cb6859daf8b994
feigebaobei <18515195415@163.com>
2019-03-28 11:24:00 +0800

    edit basic test

--- temp/index.js
+++ temp/index.js
@@ -1,5 +1,5 @@
-function addNum (a, b) {
-  return a + b
-}
+// function addNum (a, b) {
+//   return a + b
+// }
 
-module.exports = addNum
+// module.exports = addNum
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,39 @@
-var addNum = require('../temp/index.js')
-describe('test index.js', () => {
-  describe('test addNum fn', () => {
-    it('result add', () => {
-      if (addNum(1, 2) !== 3) {
-        throw new Error('不是和')
-      }
-    })
+// let expect = require('chai').expect
+// var addNum = require('../temp/index.js')
+// describe('test index.js', () => {
+//   describe('test addNum fn', () => {
+//     it('result add', () => {
+//       // if (addNum(1, 2) !== 3) {
+//       //   throw new Error('不是和')
+//       // }
+//       expect(addNum(1, 2)).to.be.equal(3)
+//     })
+//   })
+// })
+
+/*
+describe 是'测试套件'（test suite）
+name: 套件的名称。
+fn: 实际执行的函数
+it块： 测试用例。表示一个测试单元。是测试的最小单位。第一个参数：名称。第二个参数：函数。
+*/
+
+
+var expect = require('chai').expect
+
+describe('the code', () => {
+  beforeEach(() => {})
+  afterEach(() => {})
+  it('something', () => {
+    let something = 1
+    expect(something).to.exist
+  })
+  it('other something', () => {
+    var other = false
+    expect(other).to.equal(false)
+  })
+  it('other something', () => {
+    var other = false
+    expect(other).to.equal(false)
   })
 })
